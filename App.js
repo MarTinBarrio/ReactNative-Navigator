92b6c5d4f3a3eb01f7b97e7958afc0f9116d3bf6
Martin <barrio_m@yahoo.com.ar>
2023-08-02 12:00:36 -0300

    Prmier funcionamiento del drawer

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
 
-import WelcomeScreen from './screens/WelcomeScreen';
-import UserScreen from './screens/UserScreen';
+import WelcomeScreen from "./screens/WelcomeScreen";
+import UserScreen from "./screens/UserScreen";
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return <Drawer.Navigator>
-      <Drawer.Screen name ="Wellcome" component={WelcomeScreen}/>
-      <Drawer.Screen name ="User" component={UserScreen}/>
-    </Drawer.Navigator>
-  ;
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="User">
+         {/* Puedo no poner: initialRouteName="User", y en ese caso tomaría como default la
+         primer pag registrada */}
+        <Drawer.Screen name="Wellcome" component={WelcomeScreen} />
+        <Drawer.Screen name="User" component={UserScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
